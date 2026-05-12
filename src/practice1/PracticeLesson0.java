2d698ec8bb31977c26647063149b7a6bba30879e
SERG <sdahtaren@gmail.com>
2026-05-13 00:03:29 +0300

    HW

---
+++ src/practice1/PracticeLesson0.java
@@ -0,0 +1,78 @@
+package practice1;
+
+import java.util.Scanner;
+
+public class PracticeLesson0 {
+    //public class Lesson {
+    public static void main(String[] args) {
+
+ /*       int a;
+
+        a = 88/(10+12);
+        System.out.println(a);
+
+        System.out.println("***********************");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите x");
+        int x = sc.nextInt();
+        System.out.println("Введите y");
+        int y = sc.nextInt();
+        System.out.println("Введите z");
+        int z = sc.nextInt();
+        int result = x*(y*z);
+        System.out.println("Результат выполнения второго задания: "+ result);
+        System.out.println("***********************");
+*/
+
+/*      int b;
+        b = (213+258)/(217-60);
+        System.out.println("Результат выполнения третьего задания: "+ b);
+        System.out.println("***********************");
+*/
+/*
+        int i = 5;
+        int j = 2;
+        int c;
+        c = i++ * j--;
+        System.out.println("Результат выполнения четвёртого задания: "+ c);
+        System.out.println(i);
+        System.out.println(j);
+
+    }
+*/
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите a");
+        int a = sc.nextInt();
+/*    System.out.println("Введите b");
+        int b = sc.nextInt();
+        System.out.println("Введите c");
+        int c = sc.nextInt();
+*/
+/*      if (a > b) {
+            c = a - b;
+            System.out.println(c);
+        } else if (a == b) {
+            c = a + b;
+            System.out.println(c);
+        } else {
+            c = b - a;
+            System.out.println(c);
+        }
+*/
+        if (a % 3 == 0) {
+            System.out.println("Число a делится на 3");
+        } else
+            System.out.println("Число a не делится на 3");
+/*
+            if (a % 3 == 0 && a % 7 == 0) {
+                System.out.println("Число a делится на 3 и на 7");
+            } else if (a % 3 == 0) {
+                System.out.println("Число a делится на 3");
+            } else if (a % 7 == 0) {
+                System.out.println("Число a делится на 7");
+            } else System.out.println("Число a не делится на 3 и не делится на 7");*/
+        }
+}
