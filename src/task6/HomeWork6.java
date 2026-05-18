6e2a39649c5121d7f9b4090cc64b78bd83ab966d
SERG <sdahtaren@gmail.com>
2026-05-18 13:21:09 +0300

    done task3 added task with * task6

--- src/task6/HomeWork6.java
+++ src/task6/HomeWork6.java
@@ -25,7 +25,7 @@ public class HomeWork6 {
         // Выведите второй массив в консоль с помощью цикла for each.
         int[] array2 = {5, 12, 7, 9, 0};
         int[] array3 = new int[6];
-        array3[array3.length-1] = 0;
+        array3[array3.length - 1] = 0;
         for (int i = 0; i < array2.length; i++) {
             array3[i] = array2[i];
         }
@@ -35,9 +35,47 @@ public class HomeWork6 {
 
         //3.* Создать массив вручную. Заменить максимальный и минимальный элемент массива.
         // Новый минимальный элемент будет (-1000), новый максимальный элемент 1000.
-
+        int[] array1000 = {5, 12, 0, 9, 999};
+        int minElement = 100000, a = 0;
+        int maxElement = 0, b = 0;
+        for (int i = 0; i < array1000.length; i++) {
+            if (array1000[i] < minElement) {
+                minElement = array1000[i];
+                a = i;
+            }
+            if (array1000[i] > maxElement) {
+                maxElement = array1000[i];
+                b = i;
+            }
+        }
+        System.out.println(maxElement + " max " + "array1000[" + b + "]");
+        System.out.println(minElement + " min " + "array1000[" + a + "]");
+        array1000[a] = -1000;
+        array1000[b] = 1000;
+        System.out.println(array1000[b] + " max " + "array1000[" + b + "]");
+        System.out.println(array1000[a] + " min " + "array1000[" + a + "]");
 
         //4.* Создать массив (вручную). Отсортировать элементы массива в порядке возрастания.
         // Вывести полученный массив. Выполнить с помощью цикла for
+        int[] arrayFromMinToMax = {5, 12, 0, 9, 999, 3, 3852, -39, -123, 500};
+
+        for (int i = 0; i < arrayFromMinToMax.length-1; i++) {
+
+            for (int j = i + 1; j < arrayFromMinToMax.length; j++) {
+                if (arrayFromMinToMax[j] < arrayFromMinToMax[i]) {
+                    minElement = arrayFromMinToMax[j];
+                    a=j;
+                    maxElement = arrayFromMinToMax[i];
+                    arrayFromMinToMax[i] = minElement;
+                    arrayFromMinToMax[j] = maxElement;
+                }
+
+            }
+        }
+        for (int i = 0; i < arrayFromMinToMax.length; i++) {
+            System.out.print(arrayFromMinToMax[i] + ", ");
+        }
+
+
     }
 }
